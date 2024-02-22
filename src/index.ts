0ebb6e43a2847dfea175d67f71f5094a767c7952
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-02-22 17:18:06 +0100

    Add holidays

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { countryApiResponse } from "./schemas/country";
+import { holidayApiResponse } from "./schemas/holidays";
 import { languagesApiResponse } from "./schemas/languages";
 import { subdivisionsApiResponse } from "./schemas/subdivisions";
+import { parseDate, stringifyDate } from "./utils/date";
 import { requestJson } from "./utils/request";
 import { createLangQuery, serializeUrl } from "./utils/url";
 
@@ -8,9 +10,21 @@ interface HolidayOptions {
   url?: string;
 }
 
+/**
+ * Represents an SDK for interacting with the Open Holiday API.
+ */
 export class Holiday {
+  /**
+   * The base URL of the Open Holiday API.
+   * This won't have a slash at the end!
+   */
   url: string;
 
+  /**
+   * Constructs a new instance of the Holiday SDK.
+   * @param {HolidayOptions} options - The options for configuring the SDK.
+   * @param {string} options.url - The base URL of the Open Holiday API.
+   */
   constructor({ url }: HolidayOptions = {}) {
     this.url = serializeUrl(url || "https://openholidaysapi.org");
   }
@@ -19,6 +33,12 @@ export class Holiday {
    * Get all countries
    *
    * [Official docs](https://www.openholidaysapi.org/en/#countries)
+   *
+   * @param language The optional language to get the country names in.
+   *                 If a incorrect is given the data will be in english.
+   *                 If no language is given the data will be in all available languages.
+   *
+   * @returns A promise that resolves in all countries supported.
    */
   getCountries(language?: string) {
     const param = createLangQuery(language);
@@ -31,7 +51,11 @@ export class Holiday {
   /**
    * Get all languages
    *
-   * [Official docs](https://www.openholidaysapi.org/en/#languages)
+   * @param language The optional language to get the languages names in.
+   *                 If a incorrect is given the data will be in english.
+   *                 If no language is given the data will be in all available languages.
+   *
+   * @returns A promise that resolves in all langauges supported.
    */
   getLangs(language?: string) {
     const param = createLangQuery(language);
@@ -44,6 +68,10 @@ export class Holiday {
    * Get all subdivisions in a state
    *
    * [Official docs](https://www.openholidaysapi.org/en/#subdivisions)
+   *
+   * @param country The iso-code of the country which subdivisions are in question
+   * @param language The language in which to receive the subdivison names
+   * @returns A promise that resolves into a list of all subdivions that are important for holidays
    */
   getSubdivisions(country: string, language?: string) {
     const langParam = createLangQuery(language, false);
@@ -57,12 +85,88 @@ export class Holiday {
   }
 
   /**
-   * Not implemented yet!
+   * Get school holidays
+   *
+   * [Official docs](https://www.openholidaysapi.org/en/public-holidays)
+   *
+   * @param country - The iso code of the country
+   * @param startDate - The date to beginn the search from
+   * @param endDate - The date to end the search at, maximum three years after startDate
+   * @param subdivision - Optional administrative unit
+   * @param language - The optional language to get all the data in
+   *
+   * @returns A promise that resolves into the public holidays
    */
-  getPublicHolidays() {}
+  async getPublicHolidays(
+    country: string,
+    startDate: Date,
+    endDate: Date,
+    subdivision: string,
+    language?: string,
+  ) {
+    const paramsArray = [
+      createLangQuery(language, false),
+      subdivision ? `subdivisionCode=${subdivision}` : "",
+      `validFrom=${stringifyDate(startDate)}`,
+      `validTo=${stringifyDate(endDate)}`,
+      `countryIsoCode=${country}`,
+    ].filter((s) => s);
+
+    const params = `?${paramsArray.join("&")}`;
+
+    const url = `${this.url}/PublicHolidays${params}`;
+
+    const data = await requestJson(url, holidayApiResponse);
+
+    const jsNative = data.map((d) => ({
+      ...d,
+      startDate: parseDate(d.startDate),
+      endDate: parseDate(d.endDate),
+    }));
+
+    return jsNative;
+  }
 
   /**
-   * Not implemented yet!
+   * Get school holidays
+   *
+   * [Official docs](https://www.openholidaysapi.org/en/#school-holidays)
+   *
+   * @param country - The iso code of the country
+   * @param startDate - The date to beginn the search from
+   * @param endDate - The date to end the search at, maximum three years after startDate
+   * @param subdivision - Optional administrative unit
+   * @param language - The optional language to get all the data in
+   *
+   * @returns A promise that resolves into the school holidays
    */
-  getSchoolcHolidays() {}
+  async getSchoolHolidays(
+    country: string,
+    startDate: Date,
+    endDate: Date,
+    subdivision?: string,
+    language?: string,
+  ) {
+    const paramsArray = [
+      createLangQuery(language, false),
+      subdivision ? `subdivisionCode=${subdivision}` : "",
+      `validFrom=${stringifyDate(startDate)}`,
+      `validTo=${stringifyDate(endDate)}`,
+      `countryIsoCode=${country}`,
+    ].filter((s) => s);
+
+    const params = `?${paramsArray.join("&")}`;
+
+    const url = `${this.url}/SchoolHolidays${params}`;
+
+    const data = await requestJson(url, holidayApiResponse);
+
+    const jsNative = data.map((d) => ({
+      ...d,
+      startDate: parseDate(d.startDate),
+      endDate: parseDate(d.endDate),
+    }));
+
+    return jsNative;
+  }
 }
--- src/schemas/utils.ts
+++ src/schemas/utils.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 /**
  * Checks if the given string is a valid two-letter code consisting of uppercase letters only.
@@ -6,9 +6,11 @@ import { z } from 'zod'
  * @param code - The string to be checked.
  * @returns True if the string is a valid two-letter code, otherwise false.
  */
-export const isCode = (code: string) => /^[A-Z]{2}$/.test(code)
+export const isCode = (code: string) => /^[A-Z]{2}$/.test(code);
+
+export const isDate = (string: string) => /^\d{4}-\d{2}-\d{2}$/.test(string);
 
 export const nameSchema = z.object({
-      language: z.string().refine(isCode),
-      text: z.string()
-})
+  language: z.string().refine(isCode),
+  text: z.string(),
+});
